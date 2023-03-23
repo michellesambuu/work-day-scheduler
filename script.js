8c7a5378d48d570b326d72cbd0be425c4055da76
michellesambuu <michelle.sambuu@gmail.com>
2023-03-23 12:02:56 -0500

    added css classes past present future, build logic for local storage get item

--- script.js
+++ script.js
@@ -25,5 +25,33 @@
     localStorage.setItem(time, text)
   })
   
+var currentHour = dayjs().hour()
 
+$(".time-block").each(function(){
+  var planHour=parseInt($(this).attr("id").split("-")[1])
+
+  if(planHour<currentHour){
+    $(this).addClass("past")
+  }
+  else if(planHour===currentHour){
+    $(this).removeClass("past")
+    $(this).addClass("present")
+  }
+  else{
+    $(this).removeClass("past")
+    $(this).removeClass("present")
+    $(this).addClass("future")
+  }
+
+})
+
+$("#hour-9 .description").val(localStorage.getItem("hour-9"))
+$("#hour-10 .description").val(localStorage.getItem("hour-10"))
+$("#hour-11 .description").val(localStorage.getItem("hour-11"))
+$("#hour-12 .description").val(localStorage.getItem("hour-12"))
+$("#hour-13 .description").val(localStorage.getItem("hour-13"))
+$("#hour-14 .description").val(localStorage.getItem("hour-14"))
+$("#hour-15 .description").val(localStorage.getItem("hour-15"))
+$("#hour-16 .description").val(localStorage.getItem("hour-16"))
+$("#hour-17 .description").val(localStorage.getItem("hour-17"))
 });
