b012780a9767a92fb661d7550543d773cb4385ff
michellesambuu <michelle.sambuu@gmail.com>
2023-03-23 11:43:57 -0500

    added event listener and logic to add info to local storgae

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
+//   TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
   // function? How can DOM traversal be used to get the "hour-x" id of the
@@ -14,10 +13,17 @@ $(function () {
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
+  //  // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
+  $(function () {
+  $(".saveBtn").on("click",function(){
+    var time = $(this).parent().attr("id")
+    var text = $(this).siblings(".description").val()
+    localStorage.setItem(time, text)
+  })
+  
+
 });
